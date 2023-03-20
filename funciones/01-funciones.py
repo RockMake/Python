67334478301a81130e4072a2ee2b16d611ec4de8
leonardo ramires vasquez <duvanramirezvasquez@gmail.com>
2023-03-19 20:03:20 -0500

    actualizando documentos

---
+++ funciones/01-funciones.py
@@ -0,0 +1,10 @@
+def Hola(nombre, apellido="por defecto"):
+    print("hola mundo")
+    print(f"bienvenido {nombre}, {apellido}")
+
+
+Hola("juan", "pedro")
+Hola("leo")
+
+
+Hola(apellido="ramirez", nombre="leo")
